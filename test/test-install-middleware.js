a6734b3844393d373477c2ede6d08f60e4d4c3d5
christophebe <christophe.lombart@gmail.com>
2017-08-10 08:13:01 +0200

    skip the middelware install test

--- test/test-install-middleware.js
+++ test/test-install-middleware.js
@@ -11,10 +11,10 @@ describe("Test install middelware ", function() {
     //return rimraf(TEST_SITE_FOLDER_1);
   });
 
-  it.only("should install a middelware available on npm site", function() {
+  it.skip("should install a middelware available on npm site", function() {
 
     const middelwareName = "cocoons-access";
-    this.timeout(20000); 
+    this.timeout(20000);
     return install.installMiddelware(middelwareName)
           .then(() =>  console.log("Check install ....") )
           //.catch(error => {error.should.be.undefined;});
